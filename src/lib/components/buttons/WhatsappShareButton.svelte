8f0d4cbdaff51a2b3c2c5f8c746c9dde13d83d00
Lee de Verteuil <deverti@protonmail.com>
2022-10-28 14:26:19 -0400

    🔥 Stop opening new window for Whatsapp share

--- src/lib/components/buttons/WhatsappShareButton.svelte
+++ src/lib/components/buttons/WhatsappShareButton.svelte
@@ -8,29 +8,8 @@
 
 	// Constants
 	const apiUrlWithParams = buildApiUrl();
-	const windowConfigStr = buildWindowConfigStr();
 
-	// Functions
-	function buildWindowConfigStr() {
-		const windowCfg = {
-			height: 550,
-			width: 400,
-			location: 'no',
-			toolbar: 'no',
-			status: 'no',
-			directories: 'no',
-			menubar: 'no',
-			scrollbars: 'yes',
-			resizable: 'no',
-			centerscreen: 'yes',
-			chrome: 'yes'
-		};
-
-		return Object.keys(windowCfg)
-			.map((key) => `${key}=${windowCfg[key as keyof typeof windowCfg]}`)
-			.join(',');
-	}
-
+  // Functions
 	function buildApiUrl() {
 		const apiUrl =
 			browser && isMobileOrTablet()
@@ -50,7 +29,7 @@
 	}
 
 	function onClick() {
-		return window.open(apiUrlWithParams, '', windowConfigStr);
+		return window.open(apiUrlWithParams);
 	}
 
 	// Svelte class prop
