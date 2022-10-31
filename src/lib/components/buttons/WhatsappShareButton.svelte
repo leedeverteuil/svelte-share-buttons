3b564388fd2f19569b2c0ea47ced48e4e2e5e872
Lee de Verteuil <deverti@protonmail.com>
2022-10-31 09:30:03 -0400

    ♻️ Refactor whatsapp share button

--- src/lib/components/buttons/WhatsappShareButton.svelte
+++ src/lib/components/buttons/WhatsappShareButton.svelte
@@ -1,35 +1,33 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
-	import { isMobileOrTablet } from '$lib/util';
+	import { buildParamsString, isMobileOrTablet, openUrl } from '$lib/util';
 
 	// Props
 	export let url: string; // The URL to share
 	export let message: string = ''; // Optional message to send with URL
 
 	// Constants
-	const apiUrlWithParams = buildApiUrl();
+	const API_URL =
+		browser && isMobileOrTablet()
+			? 'https://api.whatsapp.com/send'
+			: 'https://web.whatsapp.com/send';
 
-  // Functions
-	function buildApiUrl() {
-		const apiUrl =
-			browser && isMobileOrTablet()
-				? 'https://api.whatsapp.com/send'
-				: 'https://web.whatsapp.com/send';
+	// Reactives
+	$: apiUrlWithParams = buildUrl(url, message);
 
+	// Functions
+	function buildUrl(url: string, message: string) {
 		const paramsObj = {
 			text: message ? message + ' ' + url : url
 		};
 
-		const paramsStr = Object.entries(paramsObj)
-			.filter(([, value]) => value ?? false)
-			.map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(String(value))}`)
-			.join('&');
+		const paramsStr = buildParamsString(paramsObj);
 
-		return paramsStr === '' ? apiUrl : `${apiUrl}?${paramsStr}`;
+		return paramsStr === '' ? API_URL : `${API_URL}?${paramsStr}`;
 	}
 
 	function onClick() {
-		return window.open(apiUrlWithParams);
+		openUrl(apiUrlWithParams);
 	}
 
 	// Svelte class prop
