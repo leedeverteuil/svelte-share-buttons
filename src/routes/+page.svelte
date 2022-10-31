a7ca0f96223089a08b6835b3cacb0dd8487b71fb
Lee de Verteuil <deverti@protonmail.com>
2022-10-31 16:07:51 -0400

    ✨ Update example page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,21 +3,47 @@
 	import ShareButtons from '$lib/components/buttons/ShareButtons.svelte';
 	import TelegramShareButton from '$lib/components/buttons/TelegramShareButton.svelte';
 	import TwitterShareButton from '$lib/components/buttons/TwitterShareButton.svelte';
+	import WebShareApiButton from '$lib/components/buttons/WebShareApiButton.svelte';
 	import WhatsappShareButton from '$lib/components/buttons/WhatsappShareButton.svelte';
 
 	// Constants
-	const url = 'www.superchex.com';
-	const text = '10 Ways to Get Better Sleep';
-	const hashtag = 'superchex';
-	const hashtags = ['superchex'];
+	const url = 'www.svelte.dev';
+	const text = 'A cool JS framework';
+	const hashtag = 'svelte';
+	const hashtags = ['svelte', 'js'];
 </script>
 
-<ShareButtons {url} {text}>
-  <!-- Inner text for web share API button -->
-  Share
+<main>
+	<h3>Web share API button and fallback example</h3>
+	<ShareButtons {url} {text}>
+		<!-- Inner text for web share API button -->
+		Web Share API
 
-  <!-- Fallback buttons -->
-	<div slot="fallback">
+		<!-- Fallback buttons -->
+		<div slot="fallback">
+			<!-- Whatsapp -->
+			<WhatsappShareButton {url} {text}>Share on WhatsApp</WhatsappShareButton>
+
+			<!-- Twitter -->
+			<TwitterShareButton {url} {text} {hashtags}>Share on Twitter</TwitterShareButton>
+
+			<!-- Facebook -->
+			<FacebookShareButton {url} {hashtag}>Share on Facebook</FacebookShareButton>
+
+			<!-- Telegram -->
+			<TelegramShareButton {url} {text}>Share on Telegram</TelegramShareButton>
+		</div>
+	</ShareButtons>
+
+	<hr />
+
+	<h3>All buttons individual examples</h3>
+
+	<!-- Fallback buttons -->
+	<div>
+		<!-- Web share API button -->
+		<WebShareApiButton {url} {text}>Web Share API</WebShareApiButton>
+
 		<!-- Whatsapp -->
 		<WhatsappShareButton {url} {text}>Share on WhatsApp</WhatsappShareButton>
 
@@ -30,9 +56,17 @@
 		<!-- Telegram -->
 		<TelegramShareButton {url} {text}>Share on Telegram</TelegramShareButton>
 	</div>
-</ShareButtons>
+</main>
 
 <style>
+	main {
+		text-align: center;
+	}
+
+  hr {
+    margin: 50px 0;
+  }
+
 	div {
 		display: flex;
 		flex-direction: row;
