6073aa7f0d58a6634aae0998051999292e9f958d
Lee de Verteuil <deverti@protonmail.com>
2022-10-31 12:04:42 -0400

    ✨ Add exports for package

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,16 @@
+import FacebookShareButton from './components/buttons/FacebookShareButton.svelte';
+import ShareButtons from './components/buttons/ShareButtons.svelte';
+import TelegramShareButton from './components/buttons/TelegramShareButton.svelte';
+import TwitterShareButton from './components/buttons/TwitterShareButton.svelte';
+import WebShareApiButton from './components/buttons/WebShareApiButton.svelte';
+import WhatsappShareButton from './components/buttons/WhatsappShareButton.svelte';
+
 // Reexport your entry components here
+export {
+	FacebookShareButton,
+	ShareButtons,
+	TelegramShareButton,
+	TwitterShareButton,
+	WebShareApiButton,
+	WhatsappShareButton
+};
