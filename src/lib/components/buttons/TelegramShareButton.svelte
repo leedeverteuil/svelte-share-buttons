0ca2e687a160178d218a00a10a53eca912782178
Lee de Verteuil <deverti@protonmail.com>
2022-10-31 13:06:18 -0400

    ♻️ Embed icon svg's in components & rewrite using vanilla css

--- src/lib/components/buttons/TelegramShareButton.svelte
+++ src/lib/components/buttons/TelegramShareButton.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Telegram from 'svelte-bootstrap-icons/lib/Telegram.svelte';
 	import { buildParamsString, openUrl } from '$lib/util';
 
 	// Props
@@ -29,9 +28,31 @@
 	}
 </script>
 
-<button
-	on:click={onClick}
-	class="text-[#37aee2] hover:text-[#2981a7] rounded-full"
-	aria-label="Share on Telegram">
-	<Telegram class="w-8 h-8" />
+<button on:click={onClick} aria-label="Share on Telegram">
+	<!-- Icon -->
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="16"
+		height="16"
+		fill="currentColor"
+		viewBox="0 0 16 16"
+		><path
+			d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z" /></svg>
 </button>
+
+<style>
+	button {
+		color: #37aee2;
+		transition: color 0.2s;
+		border-radius: 100%;
+	}
+
+	button:hover {
+		color: #2981a7;
+	}
+
+	svg {
+		width: 2rem;
+		height: 2rem;
+	}
+</style>
--- src/lib/components/buttons/WebShareApiButton.svelte
+++ src/lib/components/buttons/WebShareApiButton.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import ShareFill from 'svelte-bootstrap-icons/lib/ShareFill.svelte';
 	import { hasWebShareApi } from '$lib/util';
 	import { createEventDispatcher } from 'svelte';
 
@@ -33,11 +32,39 @@
 </script>
 
 {#if webShareSupported}
-	<button
-		on:click={onClick}
-		class="flex flex-row items-center justify-center p-2 rounded-full
-            bg-blue-500 text-blue-50 hover:bg-blue-600"
-		aria-label="Click to share">
-		<ShareFill class="w-4 h-4" />
+	<button on:click={onClick} aria-label="Click to share">
+		<!-- Icon -->
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="16"
+			height="16"
+			fill="currentColor"
+			viewBox="0 0 16 16"
+			><path
+				d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z" /></svg>
 	</button>
 {/if}
+
+<style>
+	button {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 2rem;
+		height: 2rem;
+		padding: 0.5rem;
+		border-radius: 100%;
+		background-color: #3b82f6;
+		color: #eff6ff;
+    transition: background-color 0.2s;
+	}
+
+	button:hover {
+		background-color: #2563eb;
+	}
+
+	svg {
+		width: 1rem;
+		height: 1rem;
+	}
+</style>
